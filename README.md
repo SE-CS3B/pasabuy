9ea64ea95c2c9671b0b16156742095ae49678a32
Marcial Abasola <marcial.abasolajr18@gmail.com>
2021-03-18 13:16:43 +0800

    + Added router/index.js in resources/js + Configured Package.json + Fixed vue routing
    TODO: Remove unnecesary packages in package.json
          Assigned tasks

---
+++ README.md
@@ -0,0 +1,11 @@
+Install composer packages
+``composer install``
+
+Install npm packages
+``npm install``
+
+Let laravel build and run changes --let it run in bg
+``npm run watch``
+
+Open another cmp Run development server
+``php artisan serve``
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex justify-center w-full">
+        <div class="">
+            <div class="">
+                <div class="rounded-lg border border-gray-200 shadow-lg">
+                    <div class="border border-gray-200 p-6">Example Component</div>
+
+                    <div class="p-6">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
